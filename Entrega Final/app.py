f216fa55e496cd0046659ea9701b84cc5a0615e4
Antonio-Deotti <antoniodeottineto@gmail.com>
2026-07-02 11:31:50 -0300

    Update app.py

--- Entrega Final/app.py
+++ Entrega Final/app.py
@@ -4,10 +4,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # CONEXÃO MYSQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'cnpj'
+app.config['MYSQL_HOST'] =
+app.config['MYSQL_USER'] =
+app.config['MYSQL_PASSWORD'] =
+app.config['MYSQL_DB'] =
 
 mysql = MySQL(app)
 
